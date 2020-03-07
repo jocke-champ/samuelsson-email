9910a4d52f730881c989f58f30bba23322532008
Joakim Samuelsson <40246996+jocke-champ@users.noreply.github.com>
2020-03-07 14:56:28 +0100

    Updates

---
+++ precache-manifest.dc46ca06f1e29511853641e94a0a8081.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe48bf4dfedf6742ed295d77d3225336",
+    "url": "/samuelsson-email/index.html"
+  },
+  {
+    "revision": "0e4f692d1d76f6955745",
+    "url": "/samuelsson-email/static/css/main.07f94d8c.chunk.css"
+  },
+  {
+    "revision": "81942565b1c718d0a2bf",
+    "url": "/samuelsson-email/static/js/2.afbc35d5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/samuelsson-email/static/js/2.afbc35d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4f692d1d76f6955745",
+    "url": "/samuelsson-email/static/js/main.aeb99abb.chunk.js"
+  },
+  {
+    "revision": "c93ecdf20e8c9cece26d",
+    "url": "/samuelsson-email/static/js/runtime-main.84709bc8.js"
+  },
+  {
+    "revision": "be60cd0100c2afabe3ce5099b23c10e7",
+    "url": "/samuelsson-email/static/media/slide1.be60cd01.jpg"
+  },
+  {
+    "revision": "90fe4a239f1229c6b1023f80881ad392",
+    "url": "/samuelsson-email/static/media/slide2.90fe4a23.jpg"
+  },
+  {
+    "revision": "1573f5718522a0fe34bbf89b1db64831",
+    "url": "/samuelsson-email/static/media/slide3.1573f571.jpg"
+  },
+  {
+    "revision": "6be3bcf9a343b32b2cdb6dfdf1ab2de2",
+    "url": "/samuelsson-email/static/media/slide4.6be3bcf9.jpg"
+  },
+  {
+    "revision": "af21da0be6366589d647d46628d6286c",
+    "url": "/samuelsson-email/static/media/slide5.af21da0b.jpg"
+  }
+]);
