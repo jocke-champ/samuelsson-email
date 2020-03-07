fe51b07067a79e4ef3083b5d97a23d4e0ef6a01f
Joakim Samuelsson <40246996+jocke-champ@users.noreply.github.com>
2020-03-07 14:41:15 +0100

    Let's Go

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//===========================================
+// IMPORT DEPENDENCIES
+//===========================================
+import React, { Component } from 'react';
+import Slideshow from './Slideshow';
+import slide1 from './assets/slide1.jpg';
+import slide2 from './assets/slide2.jpg';
+import slide3 from './assets/slide3.jpg';
+import slide4 from './assets/slide4.jpg';
+import slide5 from './assets/slide5.jpg';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//===========================================
+// CREATE STYLES OBJECT
+//===========================================
+const s = {
+	container: 'screenW screenH dGray col',
+	header: 'flex1 fCenter fSize2',
+	main: 'flex8 white',
+	footer: 'flex1 fCenter'
+};
+
+//===========================================
+// SLIDES DATA
+//===========================================
+const slides = [ slide1, slide2, slide3, slide4, slide5 ];
+
+//===========================================
+// APP COMPONENT
+//===========================================
+class App extends Component {
+	render() {
+		return (
+			<div className={s.container}>
+				<div className={s.header}>Slideshow</div>
+				<div className={s.main}>
+					<Slideshow slides={slides} />
+				</div>
+				<div className={s.footer}>Samuelssons slideshow</div>
+			</div>
+		);
+	}
 }
 
 export default App;
